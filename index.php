c1700db9992649723e5676880f25109cc8ce7682
Anatoliy_1980 <tttolll@rambler.ru>
2021-04-01 22:32:57 +0300

    level_1_task_2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>solution</title>
+</head>
+<body>
+
+<p>"Hello,<b>NIX Education"</b></p>
+<a href="https://github.com/opkttippp/tol/commits?author=tttolll">Публичный репозиторий</a>
+
+</body>
+</html>
